0bf2a72ac1eb5a3bd67a0eb02f6bcfde570cd148
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-08-13 13:41:23 +0900

    [add] Categoryモデル追加

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class Category < ActiveHash::Base
+  include ActiveHash::Associations
+  has_many :ranking_items
+
+  self.data = [
+    { id: 1, name: 'すべて', url: 'https://zozo.jp/ranking/all-sales.html'},
+    { id: 2, name: 'メンズ', url: 'https://zozo.jp/ranking/all-sales-men.html'},
+    { id: 3, name: 'レディース', url: 'https://zozo.jp/ranking/all-sales-women.html'},
+    { id: 4, name: 'キッズ', url: 'https://zozo.jp/ranking/all-sales-kids.html'}
+  ]
+end
