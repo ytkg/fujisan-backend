2e021a5fc5e59838db8cbb48b0d4eac3cd05288c
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-08-13 16:17:06 +0900

    [update] カテゴリーごと最新のデータを返すように

--- app/controllers/v1/ranks_controller.rb
+++ app/controllers/v1/ranks_controller.rb
@@ -1,5 +1,11 @@
 class V1::RanksController < ApplicationController
   def index
-    render json: {}
+    rank = Rank.where(category_id: params[:category_id]).last
+
+    if rank
+      render json: { date: rank.result_date, items: rank.items }, status: 200
+    else
+      render json: {}, status: 204
+    end
   end
 end
