0cc86f29264b1e686bdaa0e74c091de2f62274e5
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-08-13 12:48:45 +0900

    [add] ranksページ追加

---
+++ app/controllers/v1/ranks_controller.rb
@@ -0,0 +1,5 @@
+class V1::RanksController < ApplicationController
+  def index
+    render json: {}
+  end
+end
---
+++ test/controllers/v1/ranks_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class V1::RanksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
